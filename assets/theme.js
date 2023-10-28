80829adb68d9052ddd25de6bbc11d070cd12fb65
shamsulhuda <shamsulhuda310@gmail.com>
2023-10-28 14:29:29 +0600

    Language switcher

--- assets/theme.js
+++ assets/theme.js
@@ -1,9 +1,8 @@
-window.addEventListener("DOMContentLoaded",()=>{
-  
-    function currencyFormHandler(event){
-      event.target.form.submit();
-    }
-    document.querySelectorAll('.shopify-currency-form').forEach((elm)=>{
-      elm.addEventListener('change',currencyFormHandler);
-    })
+window.addEventListener("DOMContentLoaded",()=>{  
+  function onChangeSubmitHandler(event){
+    event.target.form.submit();
+  }
+  document.querySelectorAll('.shopify-currency-form, .language_selector').forEach((elm)=>{
+    elm.addEventListener('change',onChangeSubmitHandler);
   })
+})
